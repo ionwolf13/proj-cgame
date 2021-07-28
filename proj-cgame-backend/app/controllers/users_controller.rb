790968c54c24480533fa431beccbe7f6b11640ae
jesus_garcia <voli.scorp13@gmail.com>
2021-07-27 19:20:25 -0500

    added controllers, and user actions

--- proj-cgame-backend/app/controllers/users_controller.rb
+++ proj-cgame-backend/app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: User.all.to_json({})
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user.to_json({}) 
+    end
+       
+    def create
+        @user = User.new(user_params(:first_name, :middle_name, :last_name, :username, :age, :email, :password, :password_confirmation))
+        if @user.valid?
+            @user.save
+            
+            render json: {message: 'Application Submitted', user: @user}
+        else
+            render json: {message: @user.errors}
+        end
+       
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params(:username, :email, :email_confirmation, :password, :password_confirmation))
+        render json: @user.to_json({})
+        
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: {message: "Expelled!"}
+    end
+
+
+    private
+
+    def user_params(*args)
+        params.require(:user).permit(*args)
+    end
+
 end
