c6474c65730c0b0056648d3f4e7f98a1eb26813d
jesus_garcia <voli.scorp13@gmail.com>
2021-07-29 00:53:36 -0500

    added flexbox

--- proj-cgame-frontend/src/components/NavComponent.js
+++ proj-cgame-frontend/src/components/NavComponent.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export const NavComponent = () => {
 
     return(
         // <div className="main-cont" id='main-grid'>
             <nav className="main-cont" id='main-grid'>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Sign In</li>
-                    <li>Sign Up</li>
-                </ul>
+                    <ul className='navigation-list'>
+                        <li> <Link className='nav-links' to='/home'> Home </Link></li>
+                        <li> <Link className='nav-links' to='/about'> About </Link> </li>
+                        <li> <Link className='nav-links' to='/signin'> Sign In </Link> </li>
+                        <li> <Link className='nav-links' to='/signup'> Sign Up </Link> </li>
+                    </ul>
             </nav>
             
         // </div>
