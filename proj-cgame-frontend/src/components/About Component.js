b5705796f5efe4e5465191ac1aa0f0fc73d9a271
jesus_garcia <voli.scorp13@gmail.com>
2021-07-31 18:09:40 -0500

    grids, layout fixed, work on starting a game

--- proj-cgame-frontend/src/components/About Component.js
+++ proj-cgame-frontend/src/components/About Component.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { GameContainer } from './GameContainer'
 
 export const AboutComponent = ({games}) => {
-    
-    console.log(games)
+
     return(
         <div className="main-cont" id='about-cont'>
             <h1>Select From Any of these Games!</h1>
--- proj-cgame-frontend/src/components/GamesComponent.js
+++ proj-cgame-frontend/src/components/GamesComponent.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
+import { GameCard } from './GameCard';
 
 export const GamesComponent = ({games}) => {
+
+    const [currentChoice, setCurrentChoice] = useState({currentGame: "none"})
     console.log(games)
     return(
         <div className="main-cont" id='games-grid'>
-            <div className='second-cont' id='profile-section'>
-                <h3>All Games</h3>
+            <div className='second-cont' id='all-games-section'>
+                <h1 id='all-games-header' >All Games</h1>
+                {games.data.map(game => <GameCard game={game} setCurrentChoice={setCurrentChoice}/>)}
             </div>
-            <div className='second-cont' id='profile-section'>
-                <h3>Pick A Game</h3>
+            <div className='second-cont' id='game-info'>
+                <h1>Current Game</h1>
+                {(currentChoice.currentGame === "none")? 
+                null
+                :
+                 <div>
+                     <GameCard game={currentChoice.currentGame}/>
+                     <h4>Goals: </h4>
+                     <p>{currentChoice.currentGame.description}</p>
+                     <h4>Rules:</h4>
+                     <p>{currentChoice.currentGame.rules}</p>
+                     <button >Play Game</button>
+                 </div>
+                 }
             </div>
         </div>
     )
